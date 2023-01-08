4e45d1939d8e0e7f1e43a9e31361b753917cb55e
storkandstars <filip.storkan@gmail.com>
2023-01-08 16:06:23 +0100

    adding generic validator documentation

--- lib/src/validators/wallet_connect_uri_validator.dart
+++ lib/src/validators/wallet_connect_uri_validator.dart
@@ -8,7 +8,14 @@ import 'package:wallet_connect_uri_validator/src/wallet_connect_uri.dart';
 part 'wallet_connect_v1_uri_validator.dart';
 part 'wallet_connect_v2_uri_validator.dart';
 
+/// WalletConnect URI validator.
+///
+/// A validator for [WalletConnectUri] objects.
+/// It contains static methods for validating common URI segments.
 abstract class WalletConnectUriValidator {
+  /// Validate WalletConnect protocol
+  /// that should be always present with a value `wc`.
+  /// It throws [WalletConnectUriValidationError] if protocol is invalid.
   static void validateProtocol(String protocol) {
     if (protocol == 'wc') return;
     throw const WalletConnectUriValidationError(
@@ -16,6 +23,11 @@ abstract class WalletConnectUriValidator {
     );
   }
 
+  /// Validate WalletConnect version
+  /// Version should be always present with a value `1` or `2`.
+  /// It can be compared with [compareTo] parameter to a specific version.
+  /// It throws [WalletConnectUriValidationError] if version is 'unknown'
+  /// or does not match [compareTo] parameter.
   static void validateVersion(
     WalletConnectVersion version, {
     WalletConnectVersion? compareTo,
@@ -33,6 +45,9 @@ abstract class WalletConnectUriValidator {
     );
   }
 
+  /// Validate WalletConnect topic
+  /// Valid topic should be always present otherwise it throws ArgumentError.
+  /// Validating topic depends on the version of WalletConnect protocol.
   static Set<WalletConnectUriValidationError> validate(
     WalletConnectUri uri,
   ) {
@@ -46,8 +61,15 @@ abstract class WalletConnectUriValidator {
     throw ArgumentError.value(uri, 'uri', 'Invalid WalletConnectUri');
   }
 
+  /// Validator contains an instance of provided [WalletConnectUri].
   WalletConnectUri get uri;
+
+  /// Validator contains a set of [WalletConnectUriValidationError] errors.
   Set<WalletConnectUriValidationError> get errors;
+
+  /// Validator contains a boolean value indicating
+  /// if there are any validation errors after initialization.
+  bool get isValid => errors.isEmpty;
 
   const WalletConnectUriValidator();
 
@@ -56,6 +78,4 @@ abstract class WalletConnectUriValidator {
     if (uri is WalletConnectV2Uri) return WalletConnectV2UriValidator(uri);
     throw ArgumentError.value(uri, 'uri', 'Invalid WalletConnectUri');
   }
-
-  bool get isValid => errors.isEmpty;
 }
